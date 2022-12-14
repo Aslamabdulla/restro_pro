519dd22691dc26ecda0bda70e15223953ff2bd2b
Aslamabdulla <themanwithgunpower@gmail.com>
2022-12-14 16:34:10 +0530

    restro_app

---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: Get.height,
+        width: Get.width,
+        child: Center(
+          child: Image.asset("name"),
+        ),
+      ),
+    );
+  }
+}
